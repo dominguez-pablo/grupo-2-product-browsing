52ae6762494306a02981218dbe97a7b1fef4189b
Pablo-progr <dominguezpg.99@gmail.com>
2026-06-10 13:40:59 -0300

    TC02 UI: Navegar a la categoria Laptops

--- tests/ui/productBrowsing.ui.spec.js
+++ tests/ui/productBrowsing.ui.spec.js
@@ -6,10 +6,26 @@ test('TC01 - Navegar a categoria Phones', async ({page}) => {
 
   await page.getByRole('link', { name: 'Phones' }).click();
 
-  await page.waitForTimeout(10000);
+  await page.waitForTimeout(1000);
 
 
   const productos = page.locator('.card-title');
   await expect(productos.first()).toBeVisible();
 
 });
+
+test('TC02 - Navegar a categoria Laptops', async ({page}) => {
+  await page.goto(`${WEB}`);
+
+  await page.getByRole('link', { name: 'Laptops' }).click();
+
+  await page.waitForTimeout(1000);
+
+    const productos = page.locator('.card-title');
+    await expect(productos.first()).toBeVisible();
+});
+
+test('TC03 - Navegar a categoria Monitors', async ({page}) => {
+  await page.goto(`${WEB}`);
+
+  await page.getByRole('link', { name: 'Monitors' }).click();
